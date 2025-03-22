c68ccd53d5a7a9d8def9943f5a078d72982d03ad
ikromzzoda <git-ikromi2004@mail.ru>
2025-03-22 17:25:21 +0500

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import StudentGroup  # Импортируем модель
+from .models import Announcement #
+
+@admin.register(StudentGroup)
+class StudentGroupAdmin(admin.ModelAdmin):
+    list_display = ("name",)  # Показываем название группы в списке
+    filter_horizontal = ("students",)  # Удобный виджет для выбора студентов
+
+
+
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "is_active") 
+    search_fields = ("title", "message")  
+    list_filter = ("created_at", "is_active")  
+
+
---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from djongo import models  # ✅ Правильный импорт
+from bson import ObjectId  # MongoDB ObjectId
+from django.contrib.auth.models import User
+
+
+
+class StudentGroup(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId)  # Fix the ID type
+    name = models.CharField(max_length=100, unique=True)
+    students = models.ManyToManyField(User, related_name="student_groups",  blank=True)
+
+    class Meta:
+        verbose_name = "Student Group"
+        verbose_name_plural = "Student Groups"
+
+    def __str__(self):
+        return self.name
+
+
+
+class Announcement(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, unique=True)  # Manually assign IDs
+    title = models.CharField(max_length=100, unique=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(choices=[(0, "No"), (1, "Yes")], default=1, blank=False)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+
