ff06b89e34f1f1db3b1dd2ce6112778681244bff
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-04-19 13:21:04 +0500

    added get_all_announcements view

--- Info/urls.py
+++ Info/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('test/', views.test_api, name='test_api'),
     path('schedules/', views.get_all_schedules, name='get_all_schedules'),
+    path('announcements/', views.get_all_announcements, name='get_all_announcements'),
 ]
--- Info/views.py
+++ Info/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Schedule
+from .models import Schedule, Announcement
 
 # Create your views here.
 def test_api(request):
@@ -24,6 +24,24 @@ def get_all_schedules(request):
                 'teacher': schedule.teacher,
                 'book': schedule.book,
                 "time": schedule.time if schedule.time else [],
+            })
+            
+        return JsonResponse(data, safe=False)
+
+def get_all_announcements(request):
+    if request.method == "GET":
+        # Assuming you have a model named AnnouncementImage with a field 'image'
+        announcements = Announcement.objects.all()
+        data = []
+
+        for announcement in announcements:
+            data.append({
+                "title": announcement.title,
+                "content": announcement.content,
+                "images": announcement.images,
+                "time_posted": announcement.time_posted.strftime("%Y-%m-%d %H:%M:%S"),
+                "author": announcement.author,
+                "is_active": announcement.is_active,
             })
             
         return JsonResponse(data, safe=False)
