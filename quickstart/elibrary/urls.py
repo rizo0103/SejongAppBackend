7f65e2c0371ab3d937765c927e84001fc7009bed
ikromzzoda <git-ikromi2004@mail.ru>
2025-04-23 21:13:38 +0500

    Commit after adding .gitignore

--- quickstart/elibrary/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('elibrary/', views.get_all_books, name='get_all_books'),
-]
--- quickstart/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.get_all_users, name='get_all_users'),
-    path('groups/', views.get_all_groups, name='get_all_groups'),
-    path('login/', views.login_view, name='login_view'),
-]
